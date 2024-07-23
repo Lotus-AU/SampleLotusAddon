bf63838babbf083d9c42f12cd274953cd29b598e
Discussions <jadonbaker@icloud.com>
2024-07-23 11:12:01 -0500

    Update

--- SampleRoleAddon.cs
+++ SampleRoleAddon.cs
@@ -2,6 +2,8 @@ using SampleRoleAddon.Roles;
 using Lotus.Addons;
 using Lotus.GameModes.Standard;
 using SampleRoleAddon.Version;
+using Lotus.Roles;
+using System.Collections.Generic;
 
 namespace SampleRoleAddon;
 
@@ -9,7 +11,15 @@ public class SampleRoleAddon: LotusAddon
 {
     public override void Initialize()
     {
-        StandardRoles.AddRole(new CrewCrew());
+        // Create instances first
+        CustomRole crewCrew = new CrewCrew();
+        List<CustomRole> allRoles = new List<CustomRole>() { crewCrew };
+
+        // Register roles second
+        ExportCustomRoles(allRoles, typeof(StandardGameMode));
+
+        // Add your role to the gamemmode of your choice (Standard in this case.)
+        allRoles.ForEach(StandardRoles.AddRole);
     }
 
     public override string Name { get;} = "Sample Role Addon";
